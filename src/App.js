3a43e9074f2af1be4840fe1ea3f665f040e9daa2
selmasuicmez <selmasuicmez@gmail.com>
2022-04-02 19:08:42 +0200

    Header.js MiddleNav and BottomNav

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./home/Home";
 import Footer from "./footer/Footer";
 import About from "./about/About";
 
+
 function App() {
   return (
     <div className="App">
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,68 +1,139 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
-import logo from "../images/logo.png";
+import React ,{useState} from "react";
+import { Container, Nav,Navbar, Row, Col } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import "./Header.css";
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    button: {
-        color: "white",
-        fontSize: "15px",
-        fontWeight: "bold",
-    },
-    title: {
-        flexGrow: 1,
-    },
-    spacer: {
-        paddingLeft: "300px",
-    },
-}));
-export default function Header() {
-
-    const classes = useStyles();
-    return (
-        <div classesName={classes.root}>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <Link to="/">
-                        <img src={logo} alt="logo" />
-                    </Link>
-                    <Typography variant="h6" className={classes.title}>
-                        WW Bank
-                    </Typography>
-                    <Button className={classes.button}>ABOUT US</Button>
-                    <Button className={classes.button}>PRODUCT</Button>
-                    <Button className={classes.button}>PACKAGE</Button>
-                    <Button className={classes.button}>CONTACT</Button>
-                    <div className={classes.spacer}></div>
-                    <Link to="/login" className="header__link">
-                        <div className="header__option">
-                            <span className="header__lineOne">Hello</span>
-                            <span className="header__lineTwo ">Sign In</span>
-                        </div>
-                    </Link>
-
-                    <Link to="/register" className="header__link">
-                        <div className="header__option">
-                            <span className="header__lineOne">New User</span>
-                            <span className="header__lineTwo ">Register</span>
-                        </div>
-                    </Link>
-
-                </Toolbar>
+import logo from "../images/logo.png";
+import { Icon } from "semantic-ui-react";
 
 
-            </AppBar>
-        </div>
-    );
-}
+const Header = () => {
+  return (
+    <div className="nav-container">
+      <Container fluid className="nav-top-fluid">
+        <Container className="nav-top pt-1 pb-0">
+          <Row>
+            <Col className="d-none d-md-block">
+              <TopNav />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container className="nav-middle py-3">
+        <Row>
+          <Col>
+              <img className="nav-logo" src={logo} alt="logo" />
+          </Col>
+          <Col className="d-flex align-items-center justify-content-end">
+            <MiddleNav />
+          </Col>
+        </Row>
+      </Container>
+      <Container className="py-1">
+        <Row>
+          <Col>
+            <NavBottom/>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+const TopNav = () => {
+  const history = useHistory();
+  const handleNavClick = (eventKey) => {
+    history.push(`/${eventKey}`);
+  };
+
+  return (
+    <div>
+      <Nav variant="tabs" defaultActiveKey="cards" onSelect={handleNavClick}>
+        <Nav.Item>
+          <Nav.Link eventKey="product">Products</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="price">Prices</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="login">Sign In</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="register">Register</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>
+  );
+};
+
+const MiddleNav = () => {
+ 
+  return (
+    <div>
+  
+        <Nav className="d-flex justify-content-end">
+          <Nav.Item className="me-3">
+            <Nav.Link bsPrefix="middle-navbar">
+              <Icon
+                name="user"
+                circular
+                size="large"
+                className="d-block mb-2"
+              ></Icon>
+              Sign In
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link bsPrefix="middle-navbar">
+              <Icon
+                name="unlock alternate"
+                circular
+                size="large"
+                className="d-block mb-2 ms-2"
+              ></Icon>
+              Register
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+  
+    </div>
+  );
+  };
+
+const NavBottom = () => {
+  const history = useHistory();
+  const [activeKey, setActiveKey] = useState("home");
+
+  const handleItemClick = (eventKey) => {
+    eventKey === "home" ? history.push(`/`) : history.push(`/${eventKey}`);
+    setActiveKey(eventKey);
+  };
+
+  return (
+    <Navbar expand="md">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav
+          className="mr-auto"
+          activeKey={activeKey}
+          onSelect={handleItemClick}
+        >
+          <Nav.Link className="nav-bottom-links" eventKey="home">
+            Home
+          </Nav.Link>
+          <Nav.Link className="nav-bottom-links" eventKey="about">
+            About
+          </Nav.Link>
+
+          <Nav.Link className="nav-bottom-links" eventKey="location">
+            Location
+          </Nav.Link>
+          <Nav.Link className="nav-bottom-links" eventKey="contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+  };
+
+export default Header;
